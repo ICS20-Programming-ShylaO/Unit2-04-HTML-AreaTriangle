0ef22cc86803d180ae6b1551193edf3e347f2b13
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-14 14:12:42 +0000

    updated js

--- js/script.js
+++ js/script.js
@@ -9,17 +9,17 @@
  * This function calculates area and perimeter of the triangle.
  */
 function calculate () {
-  // input
+  // get a, b, base, and height from user
   let a = parseFloat(document.getElementById('side-a-triangle').value);
   let b = parseFloat(document.getElementById('side-b-triangle').value);
   let base = parseFloat(document.getElementById('base-triangle').value);
   let height = parseFloat(document.getElementById('height-triangle').value);
 
-  // process
+  // calculate perimeter and area
   let area = base * height / 2;
   let perimeter = (a + b + base);
 
-  // output
+  // display perimeter and area
   document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
   document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
 }
